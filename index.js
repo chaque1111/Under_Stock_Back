64794bfc6e8a7db96b6374cb797e705ca6f8483e
chaque1111 <99604927+chaque1111@users.noreply.github.com>
2023-04-25 14:13:47 -0300

    uwu

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const server = require("./src/app");
 const {conn} = require("./src/db");
 
-server.listen(3000, () => {
+server.listen(5000, () => {
   conn.sync({alter: true});
-  console.log("server is listening on port 3000");
+  console.log("server is listening on port 5000");
 });
